cf62e2e2e6a28feb6f204d6216ae214ce507bacc
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2024-06-03 22:51:38 +0300

    jsx configured

--- Client/HubClient/src/App.jsx
+++ Client/HubClient/src/App.jsx
@@ -1,59 +1,99 @@
-import { useState , useEffect } from 'react'
-import './App.css'
-import { HubConnectionBuilder} from "@microsoft/signalr";
+import { useState, useEffect } from "react";
+import "./App.css";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 function App() {
-
-  const [hubConnection, setHubConnection] = useState(null);  
-  const [message, setMessage] = useState('');
-  const [connectionId, setConnectionId] = useState('');
+  const [hubConnection, setHubConnection] = useState(null);
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState("");
+  const [connectionId, setConnectionId] = useState("");
+  const [connectionStatus, setConnectionStatus] = useState({ message: '', color: 'black' });
 
   useEffect(() => {
     createHubConnection();
-  }, [])
-  
-  const createHubConnection =async () => {
-    const hubCn = new HubConnectionBuilder().withUrl("http://localhost:5001/hubmanager").build()
+  }, []);
+
+  const createHubConnection = async () => {
+    const hubCn = new HubConnectionBuilder()
+      .withUrl("http://localhost:5001/hubmanager")
+      .withAutomaticReconnect()
+      .build();
     try {
       await hubCn.start();
-      setConnectionId(hubCn.connectionId)
-      setHubConnection(hubCn)
-    } 
-    catch (e) {
-      console.log("error", e)
+      setConnectionId(hubCn.connectionId);
+      setHubConnection(hubCn);
+    } catch (e) {
+      console.log("error", e);
     }
-  }
+  };
 
   useEffect(() => {
-    if(hubConnection && connectionId === hubConnection.connectionId){
-      hubConnection.on("ReceiveMessage",(message) => {
-        //messageList.push(message);
-        console.log(message);
-      })
-    }  
-  }, [hubConnection])
+    if (hubConnection && connectionId === hubConnection.connectionId) {
+      hubConnection.on("ReceiveMessage", (message) => {
+        if (message.trim() !== '') {
+          setMessageList((prevMessages) => [...prevMessages, message]);
+        }
+      });
+    }
+
+    hubConnection.onreconnecting(() => {
+      setConnectionStatus({ message: 'Bağlantı kuruluyor...', color: 'orange' });
+    });
+
+    hubConnection.onreconnected(() => {
+      setConnectionStatus({ message: 'Bağlantı kuruldu', color: 'green' });
+    });
+
+    hubConnection.onclose(() => {
+      setConnectionStatus({ message: 'Bağlantı kapandı', color: 'red' });
+    });
+    
+  }, [hubConnection,connectionId]);
 
   const handleChange = (event) => {
-    setMessage(event.target.value);
-  }
+    setMessage(event.target.value);    
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(hubConnection){
-      hubConnection.invoke("SendMessageAsync",message).catch(error => {
-        console.log(`Hata oluştu ${error}`);
-      })
+    if (hubConnection) {
+      hubConnection.invoke("SendMessageAsync", message)
+                    .then((res) => {
+                      setMessage('');                     
+                    })
+                    .catch((error) => {
+                      console.log(`Hata oluştu ${error}`);
+                    });
     }
-  }
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={message} onChange={handleChange} />
-        <button type="submit">Gönder</button>
-      </form>
+      <div className="container">
+        <div className="statu" style={{ display: connectionStatus.message ? 'block' : 'none', color: connectionStatus.color }}>
+          {connectionStatus.message}
+        </div>
+        <div className="inputContainer">
+          <input
+            type="text"
+            value={message}
+            onChange={handleChange}
+            className="input"
+          />
+          <button onClick={handleSubmit} className="button">
+            Gönder
+          </button>
+        </div>
+        <div className="messageList">
+          {messageList.map((msg, index) => (
+            <div key={index} className="message">
+              {msg}
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
