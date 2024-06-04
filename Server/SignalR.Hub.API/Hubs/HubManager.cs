eece7a8b3b17e5bc2128c9b966433b40e5484c70
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2024-06-04 22:52:26 +0300

    strongly types added

--- Server/SignalR.Hub.API/Hubs/HubManager.cs
+++ Server/SignalR.Hub.API/Hubs/HubManager.cs
@@ -1,23 +1,24 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Hubs.API.StronglyTypes;
 
 namespace SignalR.Hubs.API.Hubs
 {
-    public class HubManager : Hub
+    public class HubManager : Hub<IMessageClient>
     {
         static List<string> clients = new List<string>();
       
         public override async Task OnConnectedAsync()
         {
             clients.Add(Context.ConnectionId);
-            await Clients.All.SendAsync("UserJoined", Context.ConnectionId);
-            await Clients.All.SendAsync("Clients", clients);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserJoined(Context.ConnectionId);
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             clients.Remove(Context.ConnectionId);
-            await Clients.All.SendAsync("UserLeaved", Context.ConnectionId);
-            await Clients.All.SendAsync("Clients", clients);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserLeaved(Context.ConnectionId);
         }
     }
 }
---
+++ Server/SignalR.Hub.API/StronglyTypes/IMessageClient.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR.Hubs.API.StronglyTypes
+{
+    // UI funcs we'll trigger 
+    public interface IMessageClient
+    {
+        Task Clients(List<string> clients);
+        Task UserJoined(string connectionId);
+        Task UserLeaved(string connectionId);
+    }
+}
